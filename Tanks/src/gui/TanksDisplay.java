244f7a55d8fb76f0464ae78ada639ee7b8b3d983
Seungwoo Sun <seungwoos@gmail.com>
2011-11-30 13:56:14 -0700

    unbreaking what parker broke! D:<

--- Tanks/src/gui/TanksDisplay.java
+++ Tanks/src/gui/TanksDisplay.java
@@ -13,24 +13,23 @@ import javax.swing.JPanel;
 public class TanksDisplay extends JPanel {
 
 	private World world;
-	
+
 	public TanksDisplay() {
 		super(true); // It is double buffered.
-		
+
 		setPreferredSize(new Dimension(800, 600));
-		
+
 		world = new World();
 		world.addActor(new Tank(200, 300, 2, 2));
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
+		super.paint(g);
 		for (Actor a : world.getActors()) {
 			a.getDraw().draw(g, a.getX(), a.getY(), a.getRotation());
 		}
 		Toolkit.getDefaultToolkit().sync();
-		
-		super.paint(g);
 	}
-	
+
 }
