1aedf630b8851f592b26e8d28b38d7bb1eb7c21f
Travis <tstrat@email.arizona.edu>
2011-12-03 15:19:41 -0700

    Wrote in the JavaDoc for the client
    Note: No exceptions thrown in server or client are
    being handled properly (they are printing stackTraces).
    I think we should discuss how we all want each players screen to handle
    these exceptions before I just rashly decide how I think it should work.
    
    ~Travis

--- Tanks/src/server/TanksClient.java
+++ Tanks/src/server/TanksClient.java
@@ -1,12 +1,16 @@
 package server;
 import gameModel.*;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-
+import java.io.*;
+import java.net.*;
 import javax.swing.JOptionPane;
+
+
+/**
+ * This class is the client side of the network.  When called it creates a client socket at the specified
+ * ip (passed into the constructor) which will connect to a server.  It also creates a thread
+ * which handles data received from the server.
+ *
+ */
 public class TanksClient {
 	
 	private Socket client;
@@ -17,12 +21,24 @@ public class TanksClient {
 	private GameHandler gh;
 	private String ip = "";
 	
+	/**
+	 * The constructor takes in an ip and a gamehandler, then proceeds to start creating the client socket
+	 * and connect it to the server.
+	 * 
+	 * @param gameHandler - GameHandler class that the current player has
+	 * @param ip - String that contains the IP address for the socket to connect to
+	 */
 	public TanksClient(GameHandler gameHandler, String ip){
 		this.gh = gameHandler;
 		this.ip = ip;
 		start();
 	}
 	
+	/**
+	 * The start method sets up the clients socket, connects it to the server at the given ip
+	 * and sets up an output/input stream for the created socket.  Then, it proceeds to begin a
+	 * new thread dedicated to receiving and processing messages from the server.
+	 */
 	public void start(){
 		try {
 			client = new Socket(ip, 4002);
@@ -31,7 +47,6 @@ public class TanksClient {
 			is = new ObjectInputStream(client.getInputStream());
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -40,27 +55,39 @@ public class TanksClient {
 	}
 	
 	
+	/**
+	 * This thread class is created when the client socket is made.  While it is running,
+	 * the input socket waits for server data to be sent to it, then sends it to that player's
+	 * GameHandler to be used.
+	 *
+	 */
 	private class ReceiveThread extends Thread{
 		
 		public void run() {
+			
 			while (true) {
 				try {
+					
 					Command c = (Command) is.readObject();
+					
 					gh.receiveCommand(c);
 					
 				} catch (IOException e) {
-					JOptionPane
-							.showMessageDialog(null,
-									"The connection to the server has been lost. The program will now close.");
-					System.exit(0);
 					e.printStackTrace();
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-	}
+		
+	}// End of Thread class
 	
+	/**
+	 * This method sends any commands from this player from its client to the server
+	 * for the server to send to everyone else.
+	 * 
+	 * @param c - Command
+	 */
 	public void sendCommand(Command c){
 		try {
 			os.writeObject(c);
