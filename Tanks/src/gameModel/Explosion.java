a33d198de7b21d8c947acbc5b0b0590911a133d5
Seungwoo Sun <seungwoos@gmail.com>
2011-12-02 18:36:28 -0700

    removed system print

--- Tanks/src/gameModel/Explosion.java
+++ Tanks/src/gameModel/Explosion.java
@@ -3,7 +3,6 @@ package gameModel;
 public class Explosion {
 
 	public Explosion(World w, double x, double y, int damage, double radius) {
-		System.out.println(damage);
 		for(int i = 0; i < damage; i += 40) {
 			w.addActor(new FireBall(w, x, y, 2 * Math.PI * Math.random(), Math.random() * radius/ 20));
 		}
