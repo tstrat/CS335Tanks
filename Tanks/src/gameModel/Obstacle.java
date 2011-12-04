869814c1333b1f95d54e53181cb9ab3c1108f5f0
Sean <Seancmwite@gmail.com>
2011-12-04 15:34:37 -0700

    Added some more JavaDoc to Obstacle.java

--- Tanks/src/gameModel/Obstacle.java
+++ Tanks/src/gameModel/Obstacle.java
@@ -2,14 +2,44 @@ package gameModel;
 
 public class Obstacle extends Collidable{
 	
+	/**
+	 * The amount of health this tank has.
+	 */
+	
 	protected int health;
+	
+	/**
+	 * The maximum amount of health a tank can have.
+	 */
+	
 	protected int maxHealth;
 	private static int drawPriority = 10;
+	
+	
+	/**
+	 * Creates a new obstacle in the game world.
+	 * @param w - The current game world.
+	 * @param x - The obstacle's x-position.
+	 * @param y - The obstacle's y-position.
+	 * @param rotation - The direction this obstacle is facing upon creation.
+	 * 
+	 * @see Obstacle(World, double, double, double, int)
+	 */
 	
 	public Obstacle(World w, double x, double y, double rotation) {
 		super(w, x, y, rotation);
 		exists = true;
 	}
+	
+	/**
+	 * Creates a new obstacle in the game world, specifically for tanks.
+	 * Sets health, location, and direction (rotation).
+	 * @param w - The current game world
+	 * @param x - X-position
+	 * @param y - y-position
+	 * @param rotation - The direction of this obstacle (tank)
+	 * @param health - The tank's health
+	 */
 	
 	public Obstacle(World w, double x, double y, double rotation, int health) {
 		this(w, x, y, rotation);
