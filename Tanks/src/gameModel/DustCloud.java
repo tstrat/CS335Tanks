c9555843dfbb39e273a8b26ae7bd867c5e95625a
Zuragiizy <tphilyaw@arizona.email.edu>
2011-12-04 18:36:19 -0700

    Added Javadocing to both Dust Cloud and Fireball

--- Tanks/src/gameModel/DustCloud.java
+++ Tanks/src/gameModel/DustCloud.java
@@ -12,32 +12,65 @@ public class DustCloud extends Actor {
 	public int ticsLeft;
 	private static int drawPriority = 12;
 	
+	/**
+	 * A constructor for a new DustCloud object. Sets its location and it's tics to 100.
+	 * Tics are how long the Cloud lasts.
+	 * 
+	 * @param w - The current game world.
+	 * @param x - The DustCloud's x-coordinate.
+	 * @param y - DustCloud's y-coordinate.
+	 */
 	public DustCloud(World w, double x, double y) {
 		super(w, x, y, Math.random());
 		ticsLeft = 100;
 	}
 
+	/**
+	 * Updates the DustCloud's life span based on ticks.
+	 */	
 	@Override
 	public void act() {
 		ticsLeft--;
 	}
 
 	private DrawObject draw = new DrawFadingObject("dustCloud.png");
+	
+	/**
+	 * Gets the DrawObject used to draw this DustCloud. This can return null,
+	 * in which case nothing should be drawn.
+	 * 
+	 * @return A DrawObject representing this DustCloud, or null.
+	 */
 	@Override
 	public DrawObject getDraw() {
 		return draw;
 	}
 	
+	/**
+	 * Checks if the DustCloud has ran out of ticks, if so
+	 * DustCloud should cease to exist.
+	 */
 	@Override
 	public boolean exists() {
 		return ticsLeft > 0;
 	}
 
+	/**
+	 * Adds the DustCloud to the world
+	 * 
+	 * @param w The World that the Actor belongs to, is being added to.
+	 * @param x X-position in pixels.
+	 * @param y Y-position in pixels.
+	 */
 	public static void add(World w, double x, double y) {
 		if (Math.random() < 0.5)
 			w.addActor(new DustCloud(w, x, y));
 	}
 
+	/**
+	 * Returns the priority of this gun's draw. A higher priority object is drawn over
+	 * a lower priority object in the main GUI.
+	 */
 	@Override
 	public int getDrawPriority() {
 		// TODO Auto-generated method stub
--- Tanks/src/gameModel/FireBall.java
+++ Tanks/src/gameModel/FireBall.java
@@ -1,5 +1,11 @@
 package gameModel;
 
+/**
+ * Creates a fireball that spawns from an explosion.
+ * An explosion spawns fireballs from every direction once it happens.
+ * 
+ */
+
 public class FireBall extends Collidable {
 	
 	private int lifeTime;
@@ -7,6 +13,16 @@ public class FireBall extends Collidable {
 	private int damage;
 	private static int drawPriority = 25;
 	
+	/**
+	 * A constructor for a new FireBall object. Sets its location, rotation, speed, damage, and life time.
+	 * 
+	 * @param w - The current game world.
+	 * @param x - The DustCloud's x-coordinate.
+	 * @param y - DustCloud's y-coordinate.
+	 * @param rotation
+	 * @param speed - How fast the fireball moves
+	 * @param damage - How much damage the fireball deals
+	 */
 	public FireBall(World w, double x, double y, double rotation, double speed, int damage) {
 		super(w, x, y, rotation);
 		lifeTime = 20;
@@ -14,6 +30,10 @@ public class FireBall extends Collidable {
 		this.damage = damage;
 	}
 
+	/**
+	 * The collide method dictates how it interacts with objects it is colliding with. It
+	 * will damage any obstacle it collides with.
+	 */
 	@Override
 	public void collide(Collidable c) {
 		if(c instanceof Obstacle) {
@@ -22,25 +42,44 @@ public class FireBall extends Collidable {
 
 	}
 
+	/**
+	 * Updates the FireBall's position based on it's speed.
+	 * Also decreases it's lifetime if it has yet to collide with
+	 * anything.
+	 */	
 	@Override
 	public void act() {
 		lifeTime--;
 		x += speed * Math.cos(rotation);
 		y += speed * Math.sin(rotation);
-
 	}
 	
+	/**
+	 * Checks if the FireBalls lifeTime has reached zero.
+	 * If so FireBall should cease to exist.
+	 */
 	@Override
 	public boolean exists() {
 		return lifeTime > 0;
 	}
 
 	private DrawObject draw = new DrawFadingObject("fireBall.png");
+	
+	/**
+	 * Gets the DrawObject used to draw this FireBall. This can return null,
+	 * in which case nothing should be drawn.
+	 * 
+	 * @return A DrawObject representing this FireBall, or null.
+	 */
 	@Override
 	public DrawObject getDraw() {
 		return draw;
 	}
 	
+	/**
+	 * Returns the priority of this gun's draw. A higher priority object is drawn over
+	 * a lower priority object in the main GUI.
+	 */
 	@Override
 	public int getDrawPriority() {
 		// TODO Auto-generated method stub
