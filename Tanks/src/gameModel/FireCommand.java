89616bdf8063b6f0d04062ca0bbd980dc519e9be
Zuragiizy <tphilyaw@arizona.email.edu>
2011-12-04 19:15:26 -0700

    Javadoc'd FireCommand, MoveCommand, RotateCommand and World

--- Tanks/src/gameModel/FireCommand.java
+++ Tanks/src/gameModel/FireCommand.java
@@ -1,7 +1,15 @@
 package gameModel;
-
+/**
+ * Creates a new FireCommand controlling if a player has fired. 
+ *
+ */
 public class FireCommand extends Command {
 
+	/**
+	 * The constructor for a new FireCommand. Sets the player number
+	 * 
+	 * @param Player - player number
+	 */
 	public FireCommand(int Player) {
 		super(Player);
 		// TODO Auto-generated constructor stub
--- Tanks/src/gameModel/RotateCommand.java
+++ Tanks/src/gameModel/RotateCommand.java
@@ -1,15 +1,29 @@
 package gameModel;
-
+/**
+ * Creates a Command for rotating the tank with keyboard controls
+ *
+ */
 public class RotateCommand extends Command {
 
 	private double rotation;
 	
+	/**
+	 * Creates a new constructor for the RotateCommand. Sets player number and rotation.
+	 * 
+	 * @param player - Player number
+	 * @param rotation - Rotation of the tank
+	 */
 	public RotateCommand(int player, double rotation) {
 		super(player);
 		
 		this.rotation = rotation;
 	}
 	
+	/**
+	 * Gets the rotation of the Tank from the RotateCommand
+	 * 
+	 * @return RotateCommand rotation for the tank
+	 */
 	public double getRotation() {
 		return rotation;
 	}
